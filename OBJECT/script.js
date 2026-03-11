8c229d15c036b4c7bd13090ba7c26d733919b05a
Raj Singh <rajsingh22022007@gmail.com>
2026-03-11 10:21:56 +0530

    object.html

---
+++ OBJECT/script.js
@@ -0,0 +1,11 @@
+//print object
+let person = {
+    name: "Alice",
+    age: 30,
+    isMarried: true,
+    address:{
+        street : "Vinay Nagar",
+        flatNO: 201,
+    }
+};
+console.log(person);
