7b7b9c183d7ecd1f42e2c780e935f50e98f4ed1a
Raj Singh <rajsingh22022007@gmail.com>
2026-03-11 10:28:02 +0530

    object.html

--- OBJECT/script.js
+++ OBJECT/script.js
@@ -1,11 +1,18 @@
-//print object
-let person = {
-    name: "Alice",
-    age: 30,
-    isMarried: true,
+var person={
+    name:"Raj",
+    age:20, 
+    isMarried:true,
     address:{
-        street : "Vinay Nagar",
-        flatNO: 201,
+        street:'vivekanand nagar',
+        flatNO:638
     }
 };
 console.log(person);
+let person2 = new Object();
+person2.name = "Bob";
+person2.age = 25;
+person2.isMarried = false;
+person2.address = {}
+person2.address.street = "Gandhi Nagar";
+person2.address.flatNO = 101;
+console.log(person2);
